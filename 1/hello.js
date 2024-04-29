158d2e28d37ba5b422d68ea0a6fa561d8a704f27
AditiKotkar <150508095+AditiKotkar@users.noreply.github.com>
2024-04-29 04:30:28 -0700

    hello.js

--- 1/hello.js
+++ 1/hello.js
@@ -1,10 +1,11 @@
-const http = require('http');
-const PORT = 3000;
-const server = http.createServer((req, res) => {
-    res. writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hello, world!\n');
-});
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
 
-server.listen(PORT,() => {
-    console.log(`Server is running on ${PORT}`);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
