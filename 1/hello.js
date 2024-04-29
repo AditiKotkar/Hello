5d196483915aeba0e89bdff2c0db793226203023
AditiKotkar <150508095+AditiKotkar@users.noreply.github.com>
2024-04-29 04:02:01 -0700

    hello.js

---
+++ 1/hello.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    res. writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hello, world!\n');
+});
+
+server.listen(PORT,() => {
+    console.log(`Server is running on ${PORT}`);
+});
